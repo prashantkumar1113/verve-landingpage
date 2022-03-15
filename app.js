4b5bdd1858f9ee223fce2f6f71b7682cc219445f
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-03-15 17:57:34 -0500

    Added mongodb connection and .env

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db");
+
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(process.env.PORT, () => {
+    console.log(
+        `Server is running in ${process.env.NODE_ENV} mode on PORT=${process.env.PORT}`
+    );
+});
